1ab1b9749877b521d251d227a0bf9df5b03be867
SpaceDraney <merandova@gmail.com>
2025-02-11 17:36:18 +0100

    rest datas

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,30 +7,30 @@ import { Product } from '../models/product.model';
   providedIn: 'root',
 })
 export class ProductService {
-  private apiUrl = 'https://restful-api.dev';
+  private apiUrl = 'https://api.restful-api.dev';
 
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(`${this.apiUrl}/products`);
+    return this.http.get<Product[]>(`${this.apiUrl}/objects`);
   }
 
   getProduct(id: number): Observable<Product> {
-    return this.http.get<Product>(`${this.apiUrl}/products/${id}`);
+    return this.http.get<Product>(`${this.apiUrl}/objects/${id}`);
   }
 
   createProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(`${this.apiUrl}/products`, product);
+    return this.http.post<Product>(`${this.apiUrl}/objects`, product);
   }
 
   updateProduct(product: Product): Observable<Product> {
     return this.http.put<Product>(
-      `${this.apiUrl}/products/${product.id}`,
+      `${this.apiUrl}/objects/${product.id}`,
       product
     );
   }
 
   deleteProduct(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/products/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}/objects/${id}`);
   }
 }
